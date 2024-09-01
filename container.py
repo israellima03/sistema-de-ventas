ec715d2b6db813188d8b53e958f166d4b574738e
israellima03 <israelimacondori32@gmail.com>
2024-09-01 11:34:36 -0400

    mi cuarto commit da el preyecto colo que no se estan subiendo bien las focos seguimos mejorando eso :)

--- container.py
+++ container.py
@@ -2,6 +2,9 @@ from tkinter import *
 import tkinter as tk
 from ventas import Ventas
 from inventario import Inventario
+from PIL import Image, ImageTk
+
+
 class Container(tk.Frame):
     def __init__(self, padre, controlador):
         super().__init__(padre)
@@ -32,8 +35,14 @@ class Container(tk.Frame):
         frame1.pack()
         frame1.place(x=0, y=0, width=800, height=400)
 
-        btnventas = Button(frame1, bg="green", fg="black", text="ir a ventas", command=self.ventas)
+        btnventas = Button(frame1, bg="#f4b400", fg="white", font="sans 18 bold", text="ir a ventas", command=self.ventas)
         btnventas.place(x=500, y=30, width=240, height=60)
 
-        btninventario = Button(frame1, bg="blue", fg="white", text="ir a invertario", command=self.inventario)
+        btninventario = Button(frame1, bg="#c62e26", fg="white", font="sans 18 bold" ,text="ir a invertario", command=self.inventario)
         btninventario.place(x=500, y=130, width=240, height=60)
+
+        self.logo_image = Image.open("imagenes/cajaregis.avif")
+        self.logo_image = self.logo_image.resize((280,280))
+        self.logo_image = ImageTk.PhotoImage(self.logo_image)
+        self.logo_label = tk.Label(frame1, image=self.logo_image, bg="#C6D9E3")
+        self.logo_label.place(x=100, y=30)
