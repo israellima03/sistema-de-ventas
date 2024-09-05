807a22dcf44cff4657b6aae1f5a0058ff29707e9
israellima03 <israelimacondori32@gmail.com>
2024-09-05 00:37:59 -0400

    mi sexto commit hacemos un poco mas dek front end :)

--- ventas.py
+++ ventas.py
@@ -29,3 +29,18 @@ class Ventas(tk.Frame):
 
         self.entry_numero_factura = ttk.Entry(lblframe, textvariable=self.numero_factura, state="readonly", font="sans 12 bold")
         self.entry_numero_factura.place(x=100, y=5, width=80)
+
+        label_nombre = tk.Label(lblframe, text="productos: ", bg="#C6D9E3", font="sans 12 bold")
+        label_nombre.place(x=200, y=12)
+        self.entry_nombre = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_nombre.place(x=280, y=10, width=180)
+
+        label_valor = tk.Label(lblframe, text="precio", bg="#C6D9E3", font="sans 12 bold")
+        label_valor.place(x=470, y=12)
+        self.entry_valor = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_valor.place(x=540, y=10, width=180)
+
+        label_cantidad = tk.Label(lblframe, text="cantidad: ", bg="#C6D9E3", font="sans 12 bold")
+        label_cantidad.place(x=730, y=12)
+        self.entry_cantidad = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_cantidad.place(x=820, y=10)
