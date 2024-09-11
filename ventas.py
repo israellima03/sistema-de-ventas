3cc851eae3d9d25ef25deebf21349d7eca3822ed
israellima03 <israelimacondori32@gmail.com>
2024-09-10 23:50:33 -0400

    despues de algunos parciales volvemos para implemetar mas funciones para cantidad producto y venta y creando una funcion der sub total y ademas bolivia gano hoy que rwiquito :)

--- ventas.py
+++ ventas.py
@@ -44,3 +44,28 @@ class Ventas(tk.Frame):
         label_cantidad.place(x=730, y=12)
         self.entry_cantidad = ttk.Entry(lblframe, font="sans 12 bold")
         self.entry_cantidad.place(x=820, y=10)
+
+        treFrame = tk.Frame(frame2, bg="#C6D9E3")
+        treFrame.place(x=150, y=120, width=800, height=200)
+
+        scrol_y = ttk.Scrollbar(treFrame, orient=VERTICAL)
+        scrol_y.pack(side=RIGHT, fill=Y)
+
+        scrol_x = ttk,Scrollbar(treFrame, orient=HORIZONTAL)
+        scrol_x.pack(side=BOTTOM, fill=X)
+
+        self.tree = ttk.Treeview(treFrame, columns=("producto","precio","cantidad","subtotal"),show= "headings", height=10, yscrollcommand=scrol_y.set, xscrollcommand=scrol_x.set)
+        scrol_y.config(command=self.tree.yview)
+        scrol_x.config(command=self.tree.xview)
+
+        self.tree.heading("#1", text="producto")
+        self.tree.heading("#2", text="precio")
+        self.tree.heading("#3", text="cantidad")
+        self.tree.heading("#4", text="subtotal")
+
+        self.tree.column("producto", anchor="center")
+        self.tree.column("precio", anchor="center")
+        self.tree.column("cantidad", anchor="center")
+        self.tree.column("subtotal", anchor="center")
+
+        self.tree.pack(expand=True, fill=BOTH)
