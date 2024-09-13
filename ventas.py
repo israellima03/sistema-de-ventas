f48a8cb97becbd0be4a8d3d6a3517271963193de
israellima03 <israelimacondori32@gmail.com>
2024-09-12 22:13:04 -0400

    ya vamos avanzando y vemos que lo que se realizo hoy fue un trabajo de media hora que se convirtio en uno de 3 horas por una coma que tenia que ser un pinto pero pudimos agregar los button botonrd para ver pagar y ver factura vamos bien y feliz dia del programador somos el exito :)

--- ventas.py
+++ ventas.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk, messagebox
+from tkinter import Scrollbar
 
 class Ventas(tk.Frame):
     def __init__(self, parent):
@@ -51,10 +52,10 @@ class Ventas(tk.Frame):
         scrol_y = ttk.Scrollbar(treFrame, orient=VERTICAL)
         scrol_y.pack(side=RIGHT, fill=Y)
 
-        scrol_x = ttk,Scrollbar(treFrame, orient=HORIZONTAL)
-        scrol_x.pack(side=BOTTOM, fill=X)
+        scrol_x = ttk.Scrollbar(treFrame, orient=HORIZONTAL)
+        scrol_x.pack(side=BOTTOM, fill=Y)
 
-        self.tree = ttk.Treeview(treFrame, columns=("producto","precio","cantidad","subtotal"),show= "headings", height=10, yscrollcommand=scrol_y.set, xscrollcommand=scrol_x.set)
+        self.tree = ttk.Treeview(treFrame, columns=("producto","precio","cantidad","subtotal"), show= "headings", height=10, yscrollcommand=scrol_y.set, xscrollcommand=scrol_x.set)
         scrol_y.config(command=self.tree.yview)
         scrol_x.config(command=self.tree.xview)
 
@@ -69,3 +70,17 @@ class Ventas(tk.Frame):
         self.tree.column("subtotal", anchor="center")
 
         self.tree.pack(expand=True, fill=BOTH)
+
+
+        lblframe1 = LabelFrame(frame2, text="opciones", bg="#C6D9E3", font="sans 12 bold")
+        lblframe1.place(x=10, y=380, width=1060, height=100) 
+
+
+        boton_agregar = tk.Button(lblframe1, text="agregar articulo", bg="#dddddd", font="sans 12 bold")
+        boton_agregar.place(x=50, y=10, width=240, height=50)
+
+        boton_pagar = tk.Button(lblframe1, text="pagar", bg="#dddddd", font="sans 12 bold")
+        boton_pagar.place(x=400, y=10, width=240, height=50)
+
+        boton_ver_facturas = tk.Button(lblframe1, text="ver factura", bg="#dddddd", font="sans 12 bold")
+        boton_ver_facturas.place(x=750, y=10, width=240, height=50)
